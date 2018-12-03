374728bb7a4d45b1ed98512219a4184e77e1f999
mhkoji <mhkoji2+git@gmail.com>
2018-12-03 22:21:39 +0900

    Refactor

--- src/input-method/cl/emacs/stdio-server.lisp
+++ src/input-method/cl/emacs/stdio-server.lisp
@@ -2,18 +2,24 @@
   (:use :cl
         :hachee.input-method.op
         :hachee.input-method.stateless)
-  (:export :enter-loop))
+  (:export :enter-loop)
+  (:import-from :alexandria
+                :when-let))
 (in-package :hachee.input-method.emacs.stdio-server)
 
-(defun call-with-read-input (callback)
-  (loop for line = (read-line *standard-input* nil nil)
-        while line do (progn
-                        (funcall callback *standard-output* line)
-                        (force-output *standard-output*))))
+(defun process-client (kkc &key reader writer)
+  (when-let ((line (funcall reader)))
+    (let ((expr (as-expr line)))
+      (when (not (eql (expr-op expr) :quit))
+        (let ((result (kkc-eval kkc expr)))
+          (funcall writer result))
+        (process-client kkc :reader reader :writer writer)))))
+
 
 (defun enter-loop (kkc)
-  (call-with-read-input (lambda (stream line)
-    (let ((expr (as-expr line)))
-      (if (eql (expr-op expr) :quit)
-          (return-from enter-loop nil)
-          (format stream "~A~%" (kkc-eval kkc expr)))))))
+  (process-client kkc
+   :reader (lambda ()
+             (read-line *standard-input* nil nil))
+   :writer (lambda (line)
+             (format *standard-output* "~A~%" line)
+             (force-output *standard-output*))))
