717029ddc262218a2fe618c22bceecda21f9cfb0
mhkoji <mhkoji2+git@gmail.com>
2019-01-01 20:46:04 +0900

    Fcitx: fix ipc_main.cpp

--- src/input-method/fcitx/src/ipc_main.cpp
+++ src/input-method/fcitx/src/ipc_main.cpp
@@ -7,7 +7,7 @@ using namespace hachee::ipc;
 // g++ -std=c++11 ipc.cpp ipc_main.cpp
 int main() {
   Connection *conn = Connection::ConnectTo("/tmp/hachee.sock");
-  conn->Write("{\"op\": \"process-key\", \"args\": {\"code\": 97}}\n");
+  conn->Write("{\"op\": \"do-input\", \"args\": {\"code\": 97}}\n");
 
   std::string output;
   conn->ReadLine(&output);
