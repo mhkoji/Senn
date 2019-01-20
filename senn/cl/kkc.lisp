1da85c7e293ed318f10f78e7def364b70535a220
mhkoji <mhkoji2+git@gmail.com>
2019-01-20 18:08:00 +0900

    senn-emacs: load user kkc

--- senn/cl/kkc.lisp
+++ senn/cl/kkc.lisp
@@ -1,19 +1,30 @@
 ;; Provide the interface of kkc
 (defpackage :senn.kkc
   (:use :cl :hachee.kkc)
-  (:shadow :create-kkc)
   (:export :convert
            :lookup
            :word-pron
            :word-form
-           :create-kkc))
+           :load-kkc)
+  (:shadow :load-kkc))
 (in-package :senn.kkc)
 
-(defun create-kkc ()
+(defun load-user-kkc (user-homedir-pathname)
+  (when user-homedir-pathname
+    (let ((kkc-path (merge-pathnames ".senn/kkc.zip"
+                                     user-homedir-pathname)))
+      (when (cl-fad:file-exists-p kkc-path)
+        (hachee.kkc:load-kkc kkc-path)))))
+
+(defun create-system-kkc ()
   (let ((corpus-pathnames
-          (cl-fad:list-directory
-           (merge-pathnames
-            "src/kkc/data/aozora/word-pron-utf8/"
-            (asdf:system-source-directory :hachee)))))
+         (cl-fad:list-directory
+          (merge-pathnames
+           "src/kkc/data/aozora/word-pron-utf8/"
+           (asdf:system-source-directory :hachee)))))
     (log:debug "Loading: ~A" corpus-pathnames)
-    (hachee.kkc:create-kkc corpus-pathnames)))
+    (create-kkc corpus-pathnames)))
+
+(defun load-kkc (&optional user-homedir-pathname)
+  (or (load-user-kkc user-homedir-pathname)
+      (create-system-kkc)))
