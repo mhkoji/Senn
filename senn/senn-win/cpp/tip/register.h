8b6293a2d3321ad32baf978de873afba3552f7ff
mhkoji <mhkoji2+git@gmail.com>
2019-03-05 01:03:35 +0900

    senn-win: add clsid for text service

---
+++ senn/senn-win/cpp/tip/register.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <initguid.h>
+
+
+// {2EA7F750-3E6B-4F3E-A1D9-8F28E607217D}
+static const GUID CLSID_TEXT_SERVICE =
+{ 0x2ea7f750, 0x3e6b, 0x4f3e, { 0xa1, 0xd9, 0x8f, 0x28, 0xe6, 0x7, 0x21, 0x7d } };
