46ddf7850d1edfe68408987fe73389f6ad10ccad
mhkoji <mhkoji2+git@gmail.com>
2020-03-07 00:58:26 +0900

    senn-fcitx: add stateful_im_proxy_ipc_json.h

--- senn/src/cpp/senn_fcitx/stateful_im_proxy_ipc.cpp
+++ senn/src/cpp/senn_fcitx/stateful_im_proxy_ipc.cpp
@@ -1,63 +1,13 @@
 #include <fcitx/instance.h>
 #include <sstream>
-#include <picojson/picojson.h>
 
 #include "stateful_im_proxy_ipc.h"
+#include "stateful_im_proxy_ipc_json.h"
 
 namespace senn {
 namespace fcitx {
 
 namespace {
-
-void ParseConverting(picojson::value &content,
-                     senn::fcitx::views::Converting *output) {
-  const picojson::array forms = content
-    .get<picojson::object>()["forms"]
-    .get<picojson::array>();
-  for (picojson::array::const_iterator it = forms.begin();
-       it != forms.end(); ++it) {
-    output->forms.push_back(it->get<std::string>());
-  }
-
-  output->cursor_form_index = content
-    .get<picojson::object>()["cursor-form-index"]
-    .get<double>();
-
-  const picojson::array candidates = content
-    .get<picojson::object>()["cursor-form"]
-    .get<picojson::object>()["candidates"]
-    .get<picojson::array>();
-  for (picojson::array::const_iterator it = candidates.begin();
-       it != candidates.end(); ++it) {
-    output->cursor_form_candidates.push_back(it->get<std::string>());
-  }
-
-  output->cursor_form_candidate_index = content
-    .get<picojson::object>()["cursor-form"]
-    .get<picojson::object>()["candidate-index"]
-    .get<double>();
-}
-
-void ParseEditing(picojson::value &content,
-                  senn::fcitx::views::Editing *output) {
-  output->cursor_pos = content
-    .get<picojson::object>()["cursor-pos"].get<double>();
-  output->input = content
-    .get<picojson::object>()["input"].get<std::string>();
-  output->committed_input = content
-    .get<picojson::object>()["committed-input"].get<std::string>();
-
-  const picojson::array predictions = content
-    .get<picojson::object>()["predictions"]
-    .get<picojson::array>();
-  for (picojson::array::const_iterator it = predictions.begin();
-       it != predictions.end(); ++it) {
-    output->predictions.push_back(it->get<std::string>());
-  }
-
-  output->prediction_index = content
-    .get<picojson::object>()["prediction-index"].get<double>();
-}
 
 INPUT_RETURN_VALUE ParseInputReturnValue(const std::string &s) {
   if (s == "IRV_TO_PROCESS") {
@@ -118,21 +68,15 @@ StatefulIMProxyIPC::Transit(
     std::string content;
     std::getline(iss, content);
 
-    picojson::value v;
-    picojson::parse(v, content);
-
     senn::fcitx::views::Converting converting;
-    ParseConverting(v, &converting);
+    senn::fcitx::stateful_im_proxy_ipc_json::Parse(content, &converting);
     on_converting(&converting);
   } else {
     std::string content;
     std::getline(iss, content);
 
-    picojson::value v;
-    picojson::parse(v, content);
-
     senn::fcitx::views::Editing editing;
-    ParseEditing(v, &editing);
+    senn::fcitx::stateful_im_proxy_ipc_json::Parse(content, &editing);
     on_editing(&editing);
   }
 
