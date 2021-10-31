46a4aebf38060d7edffcf0a500cfca897b72e724
mhkoji <mhkoji2+git@gmail.com>
2021-10-31 18:19:26 +0900

    senn-win: Refactor

--- senn/src/cl/win/stateful-ime.lisp
+++ senn/src/cl/win/stateful-ime.lisp
@@ -16,6 +16,8 @@
 (defun history-get-form (history pron)
   (gethash pron (history-hash history)))
 
+
+;; update application state
 (defstruct state
   input-mode
   input-state
@@ -33,38 +35,39 @@
 
 (defmethod senn.im:convert ((sf-ime stateful-ime) pron
                             &key 1st-boundary-index)
-  (with-accessors ((ime stateful-ime-ime)
-                   (state stateful-ime-state)) sf-ime
-    (let ((segments (senn.im:convert
-                     ime pron :1st-boundary-index 1st-boundary-index)))
-      (with-accessors ((history state-history)) state
-        (mapcar (lambda (seg)
-                  (let* ((pron (senn.segment:segment-pron seg))
-                         (history-form (history-get-form history pron)))
-                    (if (not history-form)
-                        seg
-                        (senn.segment:make-segment
-                         :pron pron
-                         :candidates
-                         (list (senn.segment:make-candidate
-                                :form history-form
-                                :origin :history))
-                         :has-more-candidates-p t
-                         :current-index 0))))
-                segments)))))
+  (let ((segments (senn.im:convert
+                   (stateful-ime-ime sf-ime) pron
+                   :1st-boundary-index 1st-boundary-index))
+        (history (state-history (stateful-ime-state sf-ime))))
+    (mapcar (lambda (seg)
+              (let* ((pron (senn.segment:segment-pron seg))
+                     (history-form (history-get-form history pron)))
+                (if (not history-form)
+                    seg
+                    (senn.segment:make-segment
+                     :pron pron
+                     :candidates (list (senn.segment:make-candidate
+                                        :form history-form
+                                        :origin :history))
+                     :has-more-candidates-p t
+                     :current-index 0))))
+            segments)))
 
 (defmethod senn.im:lookup ((sf-ime stateful-ime) pron
                            &key prev next)
   (with-accessors ((ime stateful-ime-ime)) sf-ime
     (senn.im:lookup ime pron :next next :prev prev)))
 
+
 (defun make-ime (ime)
-  (make-instance 'stateful-ime
-                 :ime ime
-                 :state (make-state
-                         :input-mode :direct
-                         :input-state nil
-                         :history (make-history))))
+  (let ((initial-state (make-state
+                        :input-mode :direct
+                        :input-state nil
+                        :history (make-history))))
+    (make-instance 'stateful-ime
+                   :ime ime
+                   :state initial-state)))
+
 
 (defun get-input-mode (stateful-ime)
   (format nil "~A~%" (state-input-mode (stateful-ime-state stateful-ime))))
@@ -92,8 +95,8 @@
       (format nil "~A~%" (if can-process 1 0)))))
 
 (defun process-input (stateful-ime key)
-  (with-accessors ((input-mode state-input-mode)
-                   (input-state state-input-state)
+  (with-accessors ((input-state state-input-state)
+                   (input-mode state-input-mode)
                    (history state-history))
       (stateful-ime-state stateful-ime)
     (let ((result (senn.win.im.process-input:execute
@@ -101,6 +104,7 @@
       (destructuring-bind (can-process view
                            &key state committed-segments)
           result
+        ;; update application state
         (when state
           (setf input-state state))
         (when committed-segments
