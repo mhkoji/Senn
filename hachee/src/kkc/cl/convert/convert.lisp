445c7363efa02dd42295f9c967c6edb44019d242
mhkoji <mhkoji2+git@gmail.com>
2021-11-03 17:42:44 +0900

    hachee.kkc: Refactor

--- hachee/src/kkc/cl/convert/convert.lisp
+++ hachee/src/kkc/cl/convert/convert.lisp
@@ -29,13 +29,7 @@
 (defun entry-pron (entry)
   (hachee.kkc.dictionary:unit-pron (entry-unit entry)))
 
-
-(defstruct score-calc-dto
-  n-gram-model
-  unknown-word-vocabulary
-  unknown-word-n-gram-model
-  sum-probabilities-of-vocabulary-words
-  extended-dictionary)
+;;;
 
 (defun from-vocabulary-p (entry)
   (eql (entry-origin entry)
@@ -49,6 +43,13 @@
   (hachee.kkc.dictionary:make-unit
    :pron pron
    :form (hachee.ja:hiragana->katakana pron)))
+
+(defstruct score-calc-dto
+  n-gram-model
+  unknown-word-vocabulary
+  unknown-word-n-gram-model
+  sum-probabilities-of-vocabulary-words
+  extended-dictionary)
 
 (defun unknown-word-log-probability (score-calc-dto entry)
   (with-accessors ((unknown-word-vocabulary
@@ -80,8 +81,8 @@
                  (< 0 extended-dictionary-size)
                  (< 0 sum-probabilities-of-vocabulary-words))
             (let ((probability-for-extended-dictionary-words
-                    (/ sum-probabilities-of-vocabulary-words
-                       extended-dictionary-size)))
+                   (/ sum-probabilities-of-vocabulary-words
+                      extended-dictionary-size)))
               (log (+ (exp log-prob-by-unknown-word-n-gram)
                       probability-for-extended-dictionary-words)))
             log-prob-by-unknown-word-n-gram)))))
@@ -101,43 +102,44 @@
             ((from-vocabulary-p curr-entry)
              (log p))
             (t
-             (+ (log p)
-                (unknown-word-log-probability score-calc-dto
-                                              curr-entry)))))))
-                 
-                 
+             (+ (log p) (unknown-word-log-probability
+                         score-calc-dto curr-entry)))))))
 
-(defun list-entries (sub-pron &key dictionaries vocabulary)
-  (labels ((dictionary-entry->convert-entry (dictionary-entry)
-             (let* ((unit (hachee.kkc.dictionary:entry-unit
-                           dictionary-entry))
-                    (origin (hachee.kkc.dictionary:entry-origin
-                             dictionary-entry))
-                    (token (hachee.language-model.vocabulary:to-int-or-unk
-                            vocabulary
-                            (hachee.kkc.dictionary:unit->key unit))))
-               (make-entry
-                :unit unit :token token :origin origin))))
-    (let ((entries (alexandria:mappend
-                    (lambda (dict)
-                      (mapcar #'dictionary-entry->convert-entry
-                              (hachee.kkc.dictionary:lookup dict sub-pron)))
-                    dictionaries)))
+;;;
+
+(defstruct list-dto dictionaries vocabulary)
+
+(defun list-entries (list-dto sub-pron)
+  (let ((entries nil))
+    (with-accessors ((vocabulary list-dto-vocabulary)
+                     (dictionaries list-dto-dictionaries)) list-dto
+      ;; Add entries from dictionaries
+      (dolist (dict dictionaries)
+        (dolist (dict-entry (hachee.kkc.dictionary:lookup dict sub-pron))
+          (let* ((unit (hachee.kkc.dictionary:entry-unit dict-entry))
+                 (token (hachee.language-model.vocabulary:to-int-or-unk
+                         vocabulary
+                         (hachee.kkc.dictionary:unit->key unit)))
+                 (origin (hachee.kkc.dictionary:entry-origin dict-entry)))
+            (push (make-entry :unit unit :token token :origin origin)
+                  entries))))
       ;; Add unknown word entry if necessary
       (when (< (length sub-pron) 8) ;; Length up to 8
         (let ((unk-unit (make-unknown-word-unit sub-pron)))
           (when (not (some (lambda (dict)
-                             (hachee.kkc.dictionary:contains-p dict
-                                                               unk-unit))
+                             (hachee.kkc.dictionary:contains-p
+                              dict
+                              unk-unit))
                            dictionaries))
-            (push (make-entry
-                   :unit unk-unit
-                   :token (hachee.language-model.vocabulary:to-int
-                           vocabulary
-                           hachee.language-model.vocabulary:+UNK+)
-                   :origin hachee.kkc.origin:+out-of-dictionary+)
-                  entries))))
-      (nreverse entries))))
+            (let ((token (hachee.language-model.vocabulary:to-int
+                          vocabulary
+                          hachee.language-model.vocabulary:+UNK+))
+                  (origin hachee.kkc.origin:+out-of-dictionary+))
+              (push (make-entry :unit unk-unit :token token :origin origin)
+                    entries))))))
+    (nreverse entries)))
+
+;;;
 
 (defgeneric convert-n-gram-model (mixin))
 (defgeneric convert-vocabulary (mixin))
@@ -153,36 +155,42 @@
                  :unit hachee.language-model.vocabulary:+BOS+
                  :token (hachee.language-model.vocabulary:to-int
                          (convert-vocabulary convert)
-                         hachee.language-model.vocabulary:+BOS+)
+                         (hachee.kkc.dictionary:unit->key
+                          hachee.language-model.vocabulary:+BOS+))
                  :origin hachee.kkc.origin:+vocabulary+)
 
    :end-entry (make-entry
                :unit hachee.language-model.vocabulary:+EOS+
                :token (hachee.language-model.vocabulary:to-int
                        (convert-vocabulary convert)
-                       hachee.language-model.vocabulary:+EOS+)
+                       (hachee.kkc.dictionary:unit->key
+                        hachee.language-model.vocabulary:+EOS+))
                :origin hachee.kkc.origin:+vocabulary+)
 
-   :score-fn (let ((score-calc-dto
-                    (make-score-calc-dto
-                     :n-gram-model
-                     (convert-n-gram-model convert)
-                     :unknown-word-vocabulary
-                     (convert-unknown-word-vocabulary convert)
-                     :unknown-word-n-gram-model
-                     (convert-unknown-word-n-gram-model convert)
-                     :sum-probabilities-of-vocabulary-words
-                     (convert-sum-probabilities-of-vocabulary-words convert)
-                     :extended-dictionary
-                     (convert-extended-dictionary convert))))
-               (lambda (curr-entry prev-entry)
-                 (compute-score score-calc-dto curr-entry prev-entry)))
+   :score-fn
+   (let ((score-calc-dto
+          (make-score-calc-dto
+           :n-gram-model
+           (convert-n-gram-model convert)
+           :unknown-word-vocabulary
+           (convert-unknown-word-vocabulary convert)
+           :unknown-word-n-gram-model
+           (convert-unknown-word-n-gram-model convert)
+           :sum-probabilities-of-vocabulary-words
+           (convert-sum-probabilities-of-vocabulary-words convert)
+           :extended-dictionary
+           (convert-extended-dictionary convert))))
+     (lambda (curr-entry prev-entry)
+       (compute-score score-calc-dto curr-entry prev-entry)))
 
    :list-entries-fn
-   (let ((vocab (convert-vocabulary convert))
-         (dicts (list (convert-word-dictionary convert)
-                      (convert-extended-dictionary convert))))
+   (let ((list-dto (make-list-dto
+                    :vocabulary
+                    (convert-vocabulary convert)
+                    :dictionaries
+                    (list (convert-word-dictionary convert)
+                          (convert-extended-dictionary convert)))))
      (lambda (pron)
-       (list-entries pron :dictionaries dicts :vocabulary vocab)))
+       (list-entries list-dto pron)))
 
    :1st-boundary-index 1st-boundary-index))
