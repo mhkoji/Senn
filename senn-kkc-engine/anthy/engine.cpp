304f832b3dfbab48d1efb2496a64a46b573b0769
mhkoji <mhkoji2@gmail.com>
2022-01-11 21:06:13 +0900

    Update anthy kkc engine (#19)
    * Show all candidates
    
    * Define AnthyConvert
    
    * Implement CONVERT protocol
    
    * Implement LOOKUP protocol
    
    * Add Makefile
    
    * Rename loop method

--- senn-kkc-engine/anthy/engine.cpp
+++ senn-kkc-engine/anthy/engine.cpp
@@ -2,7 +2,10 @@
 #include <anthy/anthy.h>
 #include <fcitx-utils/utf8.h>
 #include <iostream>
+#include <picojson/picojson.h>
+#include <stdexcept>
 #include <string.h>
+#include <vector>
 
 std::string utf8_string_substr(const std::string &s, size_t start, size_t len) {
   char *cs = strdup(s.c_str());
@@ -13,19 +16,19 @@ std::string utf8_string_substr(const std::string &s, size_t start, size_t len) {
   return result;
 }
 
-// sudo apt install -y libanthy-dev
-// g++ engine.cpp -lanthy -lfcitx-utils
-// ./a.out
-int main(void) {
-  anthy_init();
+struct Segment {
+  std::string pron;
+  std::vector<std::string> candidate_forms;
+};
 
-  anthy_context_t anthy_context = anthy_create_context();
-
-  std::string pron = "きょうhaよいてんきです。";
+void AnthyConvert(anthy_context_t anthy_context, const std::string &pron,
+                  std::vector<Segment> *segs) {
   anthy_set_string(anthy_context, pron.c_str());
 
   struct anthy_conv_stat conv_stat;
   anthy_get_stat(anthy_context, &conv_stat);
+
+  segs->resize(conv_stat.nr_segment);
 
   unsigned int seg_start_in_pron = 0;
   for (int nth_seg = 0; nth_seg < conv_stat.nr_segment; nth_seg++) {
@@ -34,22 +37,97 @@ int main(void) {
 
     std::string sub_pron =
         utf8_string_substr(pron, seg_start_in_pron, seg_stat.seg_len);
+    (*segs)[nth_seg].pron = sub_pron;
 
-    std::string form;
-    {
-      int nth_cand = 0;
+    (*segs)[nth_seg].candidate_forms.resize(seg_stat.nr_candidate);
+    for (int nth_cand = 0; nth_cand < seg_stat.nr_candidate; nth_cand++) {
       int len = anthy_get_segment(anthy_context, nth_seg, nth_cand, NULL, 0);
       char buf[len + 1];
       anthy_get_segment(anthy_context, nth_seg, nth_cand, buf, len + 1);
       buf[len + 1] = '\0';
-      form = buf;
+      (*segs)[nth_seg].candidate_forms[nth_cand] = buf;
     }
-
-    std::cout << form << "(" << sub_pron << ")" << std::endl;
 
     seg_start_in_pron += seg_stat.seg_len;
   }
+}
 
-  anthy_release_context(anthy_context);
-  anthy_quit();
+void Loop(anthy_context_t anthy_context) {
+  std::vector<Segment> segments;
+
+  while (true) {
+    std::string req_string;
+    std::getline(std::cin, req_string);
+
+    picojson::value req;
+    picojson::parse(req, req_string);
+
+    std::string op = req.get<picojson::object>()["op"].get<std::string>();
+
+    if (op == "CONVERT") {
+      segments.clear();
+      std::string pron = req.get<picojson::object>()["args"]
+                             .get<picojson::object>()["pron"]
+                             .get<std::string>();
+      AnthyConvert(anthy_context, pron, &segments);
+
+      picojson::array items;
+      for (size_t s = 0; s < segments.size(); s++) {
+        picojson::object item;
+        item["pron"] = picojson::value(segments[s].pron);
+        item["form"] = picojson::value(segments[s].candidate_forms[0]);
+        items.push_back(picojson::value(item));
+      }
+      picojson::value resp(items);
+      std::cout << resp << std::endl;
+    } else if (op == "LOOKUP") {
+      std::string pron = req.get<picojson::object>()["args"]
+                             .get<picojson::object>()["pron"]
+                             .get<std::string>();
+      picojson::array items;
+      for (size_t s = 0; s < segments.size(); s++) {
+        if (pron == segments[s].pron) {
+          for (size_t c = 1; c < segments[s].candidate_forms.size(); c++) {
+            picojson::object item;
+            item["form"] = picojson::value(segments[s].candidate_forms[c]);
+            items.push_back(picojson::value(item));
+          }
+          break;
+        }
+      }
+      picojson::value resp(items);
+      std::cout << resp << std::endl;
+    } else {
+      throw std::runtime_error("Invalid op: " + op);
+    }
+  }
+}
+
+class AnthyReleaer {
+public:
+  ~AnthyReleaer() { anthy_quit(); }
+};
+
+class AnthyContextReleaser {
+public:
+  AnthyContextReleaser(anthy_context_t anthy_context)
+      : anthy_context_(anthy_context) {}
+
+  ~AnthyContextReleaser() { anthy_release_context(anthy_context_); }
+
+private:
+  const anthy_context_t anthy_context_;
+};
+
+// sudo apt install -y libanthy-dev
+// g++ engine.cpp -lanthy -lfcitx-utils -I ../../senn/third-party/
+// ./a.out
+int main(void) {
+  anthy_init();
+  AnthyReleaer anthy_releaser();
+
+  anthy_context_t anthy_context = anthy_create_context();
+  AnthyContextReleaser anthy_context_releaser(anthy_context);
+
+  Loop(anthy_context);
 }
