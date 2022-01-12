fb0d3aa08958e453629e418a6c99f17c77558497
mhkoji <mhkoji2@gmail.com>
2022-01-13 00:43:42 +0900

    Report coverage

--- senn/bin/run-fcitx-tests.sh
+++ senn/bin/run-fcitx-tests.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+rm -rf ./coverage && \
 ros run \
-    -s senn-fcitx-test \
+    -e "(require :sb-cover)" \
+    -e "(declaim (optimize sb-cover:store-coverage-data))" \
+    -e "(asdf:oos 'asdf:load-op :senn :force t)" \
+    -e "(asdf:oos 'asdf:load-op :senn-fcitx :force t)" \
+    -e "(asdf:oos 'asdf:load-op :senn-fcitx-test :force t)" \
     -e "(senn.t.scenario.fcitx:run)" \
+    -e "(sb-cover:report \"coverage/\")" \
     -q
