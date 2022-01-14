872ec4a11e5bc78aaf7a7058ce5556eafbc65c3b
mhkoji <mhkoji2@gmail.com>
2022-01-14 23:27:05 +0900

    Update ibus server

--- senn/src/bin/ibus-server.lisp
+++ senn/src/bin/ibus-server.lisp
@@ -1,12 +1,23 @@
 (defpackage :senn.bin.ibus-server
   (:use :cl)
-  (:export :tcp-run))
+  (:export :tcp-run
+           :tcp-run-engine))
 (in-package :senn.bin.ibus-server)
+
+(defun ime-client-loop (client ime)
+  (labels ((handle (req)
+             (senn.ibus.server:handle-request ime req)))
+    (senn.server:client-loop client :handle-fn #'handle)))
 
 (defun tcp-run (kkc)
   (senn.server.tcp:start-server
    (lambda (client)
-     (let ((sf-ime (senn.ibus.stateful-ime:hachee-make-ime kkc)))
-       (labels ((handle (req)
-                  (senn.ibus.server:handle-request sf-ime req)))
-         (senn.server:client-loop client :handle-fn #'handle))))))
+     (let ((ime (senn.ibus.stateful-ime:hachee-make-ime kkc)))
+       (ime-client-loop client ime))))
+
+(defun tcp-run-engine (runner)
+  (senn.server.tcp:start-server
+   (lambda (client)
+     (let ((ime (senn.ibus.stateful-ime:engine-make-ime runner)))
+       (unwind-protect (ime-client-loop client ime)
+         (senn.ibus.stateful-ime:engine-close-ime ime))))))
