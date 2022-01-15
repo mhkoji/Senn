7a207abc2fb5e8ebc96524a3973cb8a6492cedb8
mhkoji <mhkoji2@gmail.com>
2022-01-15 10:02:48 +0900

    Commonalize ime class for fcitx (#33)

--- senn/src/fcitx/stateful-ime-engine.lisp
+++ senn/src/fcitx/stateful-ime-engine.lisp
@@ -4,20 +4,13 @@
            :close-ime))
 (in-package :senn.fcitx.stateful-ime-engine)
 
-(defclass ime (senn.fcitx.stateful-ime:ime)
-  ((engine-kkc :initarg :engine-kkc)))
-
-(defmethod senn.fcitx.im:ime-kkc ((ime ime))
-  (slot-value ime 'engine-kkc))
-
 (defun make-ime (engine-runner)
-  (make-instance 'ime
-   :state (senn.fcitx.stateful-ime:make-initial-state)
-   :engine-kkc (make-instance 'senn.im.kkc.engine:kkc
-                :engine-store
-                (senn.im.kkc.engine:make-engine-store
-                 :engine (senn.im.kkc.engine:run-engine engine-runner)
-                 :engine-runner engine-runner))))
+  (senn.fcitx.stateful-ime:make-ime
+   :kkc (make-instance 'senn.im.kkc.engine:kkc
+         :engine-store
+         (senn.im.kkc.engine:make-engine-store
+          :engine (senn.im.kkc.engine:run-engine engine-runner)
+          :engine-runner engine-runner))))
 
 (defun close-ime (ime)
-  (senn.im.kkc.engine:close-kkc (slot-value ime 'engine-kkc)))
+  (senn.im.kkc.engine:close-kkc (senn.fcitx.im:ime-kkc ime)))
