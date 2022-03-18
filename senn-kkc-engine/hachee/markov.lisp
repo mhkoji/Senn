5e422efa756c3333eee277393cc6f4f9a6f27374
mhkoji <mhkoji2@gmail.com>
2022-03-18 16:31:08 +0900

    fcitx-senn: Use hachee-lm as default kkc-engine

--- senn-kkc-engine/hachee/markov.lisp
+++ senn-kkc-engine/hachee/markov.lisp
@@ -1,21 +1,25 @@
 (defpackage :senn-kkc-engine.hachee.markov
   (:use :cl)
-  (:export :main))
+  (:export :set-kkc
+           :main))
 (in-package :senn-kkc-engine.hachee.markov)
 
-(defun read-kkc ()
-  ;; We suppose that all the dependency files are put in ~/senn-data.
-  (hachee.kkc.impl.markov.read:read-kkc
-   :path-word-int-str "~/senn-data/word/int-str.txt"
-   :path-word-1gram   "~/senn-data/word/cost-1gram.tsv"
-   :path-word-2gram   "~/senn-data/word/cost-2gram.tsv"
-   :path-char-int-str "~/senn-data/char/int-str.txt"
-   :path-char-1gram   "~/senn-data/char/cost-1gram.tsv"
-   :path-char-2gram   "~/senn-data/char/cost-2gram.tsv"
-   :path-in-dict      "~/senn-data/in-dict.tsv"))
+(defun read-kkc (markov-dir)
+  (labels ((make-path (path)
+             (format nil "~A~A" markov-dir path)))
+    (hachee.kkc.impl.markov.read:read-kkc
+     :path-word-int-str (make-path "word/int-str.txt")
+     :path-word-1gram   (make-path "word/cost-1gram.tsv")
+     :path-word-2gram   (make-path "word/cost-2gram.tsv")
+     :path-char-int-str (make-path "char/int-str.txt")
+     :path-char-1gram   (make-path "char/cost-1gram.tsv")
+     :path-char-2gram   (make-path "char/cost-2gram.tsv")
+     :path-in-dict      (make-path "in-dict.tsv"))))
 
-(defvar *kkc*
-  (read-kkc))
+(defvar *kkc*)
+
+(defun set-kkc (&optional (markov-dir "~/senn-data/"))
+  (setq *kkc* (read-kkc markov-dir)))
 
 (defun main ()
   (senn-kkc-engine.hachee:run *kkc*
