d484bf8e499169af820f524f3360d69191b9c1cb
mhkoji <mhkoji2@gmail.com>
2022-03-19 09:42:54 +0900

    ibus-senn: Use hachee-lm as default kkc-engine

--- docker/ibus-senn/build.sh
+++ docker/ibus-senn/build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 DIR=../../
-docker build $DIR -f ./kkc-builder.dockerfile -t kkc-builder && \
-docker build $DIR -f ./ecl-builder.dockerfile -t ecl-builder && \
-docker build $DIR -f ./deb-builder.dockerfile -t deb-builder && \
+docker build $DIR -f ../kkc-engine/hachee-lm.dockerfile \
+       -t kkc-builder && \
+docker build $DIR -f ./ecl-builder.dockerfile \
+       -t ecl-builder && \
+docker build $DIR -f ./deb-builder.dockerfile \
+       -t deb-builder && \
 docker run -v $PWD:/host --rm -t deb-builder
