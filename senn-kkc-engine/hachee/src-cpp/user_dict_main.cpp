2debe7e97ef1316992aacc10e5cce958af265afc
mhkoji <mhkoji2+git@gmail.com>
2022-04-15 22:04:22 +0900

    user-dict: Change not found message

--- senn-kkc-engine/hachee/src-cpp/user_dict_main.cpp
+++ senn-kkc-engine/hachee/src-cpp/user_dict_main.cpp
@@ -6,7 +6,6 @@
 #include <sys/types.h>
 #include <pwd.h>
 
-
 // g++ user_dict_main.cpp user_dict.cpp -std=c++11
 // g++ -shared -fPIC user_dict.cpp -o user_dict.so -std=c++11
 int main(void) {
@@ -14,7 +13,7 @@ int main(void) {
   std::string user_dict_path = home_dir + "/.senn/user-dict.txt";
   user_dict_t ud = user_dict_load(user_dict_path.c_str());
   if (ud == NULL) {
-    std::cout << "no such file" << std::endl;
+    std::cout << "user-dict file not found: " << user_dict_path << std::endl;
     return 0;
   }
 
