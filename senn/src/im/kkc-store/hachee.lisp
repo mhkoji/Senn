fba8c61a15076ce47a772d912225a5a1d7c89a54
mhkoji <mhkoji2+git@gmail.com>
2022-04-16 08:14:37 +0900

    senn: Remove user-dict dependency

--- senn/src/im/kkc-store/hachee.lisp
+++ senn/src/im/kkc-store/hachee.lisp
@@ -7,17 +7,9 @@
   kkc
   hachee-impl-lm-kkc)
 
-(defun build-ex-dict (user-dict hachee-impl-lm-kkc)
-  (when user-dict
-    (hachee.kkc.impl.lm.ex-dict-builder:build
-     user-dict
-     (hachee.kkc.impl.lm:kkc-word-dictionary hachee-impl-lm-kkc))))
-
 (defun make-kkc (hachee-impl-lm-kkc)
   (make-instance 'senn.im.kkc.hachee:kkc
-   :hachee-impl-lm-kkc hachee-impl-lm-kkc
-   :extended-dictionary (build-ex-dict (senn-user-dict:read-file)
-                                       hachee-impl-lm-kkc)))
+   :hachee-impl-lm-kkc hachee-impl-lm-kkc))
 
 (defun make-store (hachee-impl-lm-kkc)
   (%make-store :kkc (make-kkc hachee-impl-lm-kkc)
