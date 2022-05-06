fb421ba5faf56db5a0c5e3c69a4b5faf71d2760d
mhkoji <mhkoji2+git@gmail.com>
2022-05-06 23:54:44 +0900

    win: Use kkc-engine instead of named-pipe kkc

--- senn/src/bin/win-server.lisp
+++ senn/src/bin/win-server.lisp
@@ -3,18 +3,27 @@
   (:export :run))
 (in-package :senn.bin.win-server)
 
-(defun run ()
+(defun make-engine-runner ()
+  (senn.im.kkc.engine:make-engine-runner
+   :program (merge-pathnames ".senn/kkc/engine"
+			     (user-homedir-pathname))))
+
+(defun run-internal (kkc)
   (senn-ipc.server.named-pipe:run
    (lambda (client)
-     ;; TODO: verify that the kkc server is valid.
-     (let ((kkc (senn.im.kkc.named-pipe:make-kkc-and-connect
-		 "\\\\.\\Pipe\\senn\\senn-kkc-engine")))
-       (unwind-protect
-	    (let ((ime (senn.win.stateful-ime:make-ime
-			:kkc kkc
-			:predictor nil)))
-	      (labels ((handle (req)
-			 (senn.win.server:handle-request ime req)))
-		(senn-ipc.server:client-loop client :handle-fn #'handle)))
-	 (senn.im.kkc.named-pipe:close-kkc kkc))))
+     (unwind-protect
+	  (let ((ime (senn.win.stateful-ime:make-ime
+		      :kkc kkc
+		      :predictor nil)))
+	    (labels ((handle (req)
+		       (senn.win.server:handle-request ime req)))
+	      (senn-ipc.server:client-loop client :handle-fn #'handle)))
+       (senn.im.kkc.named-pipe:close-kkc kkc)))
    :pipe-name "\\\\.\\Pipe\\senn\\senn\\bin\\win-server\\run"))
+
+(defun run ()
+  (let ((kkc (senn.im.kkc.engine:make-kkc-and-run
+	      (make-engine-runner))))
+    (unwind-protect
+	 (run-internal kkc)
+      (senn.im.kkc.engine:close-kkc kkc))))
