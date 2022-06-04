f1e34691b9f81253ca8afcecb0f91a82e3f5b70b
mhkoji <mhkoji2+git@gmail.com>
2022-06-04 15:55:50 +0900

    Senn ime log (#114)
    * fcitx: Change log
    
    * ibus: Change log

--- senn/src/lib/ibus-engine.lisp
+++ senn/src/lib/ibus-engine.lisp
@@ -1,12 +1,13 @@
 (in-package :senn.lib.ibus)
 
 (defun make-ime (engine-path)
+  (log:info "Making IME ...")
   (let ((kkc (senn.im.kkc.engine:make-kkc-and-run
               (senn.im.kkc.engine:make-engine-runner
                :program engine-path))))
     (senn.ibus.stateful-ime:make-ime :kkc kkc)))
 
 (defun close-ime (ime)
-  (log:info "IME closing...")
+  (log:info "Closing IME ...")
   (let ((kkc (senn.ibus.stateful-ime:ime-kkc ime)))
     (senn.im.kkc.engine:close-kkc kkc)))
