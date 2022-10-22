e548aaacc6435c648fc4974f2bffb0336ab6a4ae
mhkoji <mhkoji2+git@gmail.com>
2022-10-22 21:55:54 +0900

    win: Update lib

--- senn/src/lib/win-engine.lisp
+++ senn/src/lib/win-engine.lisp
@@ -1,17 +1,30 @@
 (in-package :senn.lib.win)
 
-(defun make-ime (engine-path)
-  (log:info "Making IME ...")
-  (let ((engine-runner (senn.im.kkc.engine:make-engine-runner
-                        :program engine-path)))
-    (senn.win.stateful-ime:make-ime
-     :kkc (make-instance 'senn.im.kkc.engine:kkc
+(defvar *kkc*
+  nil)
+
+(defun init (engine-path)
+  (let ((engine-runner
+         (senn.im.kkc.engine:make-engine-runner
+          :program engine-path)))
+    (setq *kkc*
+          (make-instance 'senn.im.kkc.engine:kkc
            :engine-store
            (senn.im.kkc.engine:make-engine-store
             :engine (senn.im.kkc.engine:run-engine engine-runner)
-            :engine-runner engine-runner))
-     :predictor nil)))
+            :engine-runner engine-runner)))))
+
+(defun destroy ()
+  (when *kkc*
+    (senn.im.kkc.engine:close-kkc *kkc*)))
+
+(defun make-ime ()
+  (log:info "Making IME ...")
+  (assert *kkc*)
+  (senn.win.stateful-ime:make-ime
+   :kkc *kkc*
+   :predictor nil))
 
 (defun close-ime (ime)
-  (log:info "IME closing...")
-  (senn.im.kkc.engine:close-kkc (senn.win.stateful-ime:ime-kkc ime)))
+  (declare (ignore ime))
+  (log:info "Closing IME ..."))
--- senn/t/win/lib.lisp
+++ senn/t/win/lib.lisp
@@ -4,17 +4,17 @@
 (in-package :senn.t.win.lib)
 
 (defmacro make-ime (&key test)
-  `(let ((engine-path (merge-pathnames
-                       "bin/mock-kkc-engine.py"
-                       (asdf:system-source-directory :senn-win))))
-     (let ((ime (senn.lib.win:make-ime engine-path)))
-       (unwind-protect
-            (let ((state (senn.im.converting:convert ime "きょうは")))
-              (,test (equal
-                      (senn.t.im-util:converting-state-segment-strings
-                       state)
-                      '("今日/きょう" "は/は"))))
-         (senn.lib.win:close-ime ime)))))
+  `(progn
+     (senn.lib.win:init (merge-pathnames
+                         "bin/mock-kkc-engine.py"
+                         (asdf:system-source-directory :senn-win)))
+     (unwind-protect
+          (let* ((ime (senn.lib.win:make-ime))
+                 (state (senn.im.converting:convert ime "きょうは")))
+            (,test (equal
+                    (senn.t.im-util:converting-state-segment-strings state)
+                    '("今日/きょう" "は/は"))))
+       (senn.lib.win:destroy))))
 
 (senn.t.win:add-tests
  :lib
