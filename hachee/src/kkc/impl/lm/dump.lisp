493be6b6ba56744a9c53a70e6caa6f06824bd343
mhkoji <mhkoji2+git@gmail.com>
2022-12-04 13:10:30 +0900

    hachee-kkc: Update dump

--- hachee/src/kkc/impl/lm/dump.lisp
+++ hachee/src/kkc/impl/lm/dump.lisp
@@ -1,8 +1,18 @@
 (defpackage :hachee.kkc.impl.lm.dump
-  (:use :cl))
+  (:use :cl)
+  (:export :kkc-class-vocabulary
+           :kkc-class-model
+           :kkc-unk-vocabulary
+           :kkc-unk-model
+           :execute))
 (in-package :hachee.kkc.impl.lm.dump)
 
-(defun dump-vocabulary (vocab classifier out-path)
+(defgeneric kkc-class-vocabulary (kkc))
+(defgeneric kkc-class-model (kkc))
+(defgeneric kkc-unk-vocabulary (kkc))
+(defgeneric kkc-unk-model (kkc))
+
+(defun dump-class-vocabulary (vocab classifier out-path)
   (let ((vocab-size (hachee.language-model.vocabulary::vocabulary-size
                      vocab))
         (to-int-map (hachee.language-model.vocabulary::vocabulary-to-int-map
@@ -18,39 +28,115 @@
                     classifier tok))
               (str (gethash tok to-str-map)))
           (format out "~A~A~A~A~A~%"
-                  tok #\tab str #\tab cls))))))
+                  str #\tab tok #\tab cls))))))
 
-(defun dump-class-model (class-model vocab class-trans-path token-count-path)
-  (with-open-file (out class-trans-path :direction :output)
-    (let* ((to-class-map
-            (hachee.language-model.n-gram::classifier-to-class-map
-             (hachee.language-model.n-gram::class-model-classifier
-              class-model)))
-           (class-list
-            (remove-duplicates
-             (sort (loop for class being the hash-value of to-class-map
-                         collect class)
-                   #'<)
-             :test #'=)))
-      (dolist (prev class-list)
-        (let ((hist (list prev)))
-          (dolist (next class-list)
-            (let ((prob
-                   (hachee.language-model.n-gram::interpolated-probability
-                    class-model next hist)))
-              (when (/= prob 0)
-                (format out "~A~A~A~A~A~%"
-                        prev #\tab next #\tab prob))))))))
-  (with-open-file (out token-count-path :direction :output)
-    (let ((vocab-size (hachee.language-model.vocabulary::vocabulary-size
-                       vocab))
-          (token-freq (hachee.language-model.n-gram::class-model-token-freq
-                       class-model))
-          (bos-token (hachee.language-model.vocabulary:to-int
-                      vocab hachee.language-model.vocabulary:+BOS+)))
-      (dotimes (token vocab-size)
-        (let ((count (if (= token bos-token)
-                         0
-                         (gethash token token-freq))))
+(defun dump-class-model (class-model
+                         class-weights-path
+                         class-ngram-counts-path
+                         class-word-counts-path)
+  (let ((weights (hachee.language-model.n-gram::model-weights class-model)))
+    (assert (= (length weights) 2))
+    (with-open-file (out class-weights-path :direction :output)
+      (format out "~A~A~A~%" (nth 0 weights) #\tab (nth 1 weights))))
+  (let* ((token-freq (hachee.language-model.n-gram::class-model-token-freq
+                      class-model))
+         (token-list (sort (loop for token being the hash-key of token-freq
+                                 collect token)
+                           #'<)))
+    (with-open-file (out class-word-counts-path :direction :output)
+      (dolist (token token-list)
+        (let ((count (gethash token token-freq)))
           (assert count)
-          (format out "~A~A~A~%" token #\tab count))))))
+          (format out "~A,~A~%" count token)))))
+  (let* ((to-class-map
+          (hachee.language-model.n-gram::classifier-to-class-map
+           (hachee.language-model.n-gram::class-model-classifier
+            class-model)))
+         (class-list
+          (remove-duplicates
+           (sort (loop for class being the hash-value of to-class-map
+                       collect class)
+                 #'<)
+           :test #'=))
+         (freq
+          (hachee.language-model.n-gram::model-freq class-model)))
+    (with-open-file (out class-ngram-counts-path :direction :output)
+      ;; 0-gram
+      (let ((count (hachee.language-model.freq:get-count freq nil)))
+        (format out "~A~%" count))
+      ;; 1-gram
+      (dolist (class class-list)
+        (let ((count (hachee.language-model.freq:get-count
+                      freq (list class))))
+          (assert count)
+          (format out "~A,~A~%" count class)))
+      ;; 2-gram
+      (dolist (prev class-list)
+        (dolist (next class-list)
+          (let ((count (hachee.language-model.freq:get-count
+                        freq (list prev next))))
+            (when count
+              (format out "~A,~A,~A~%" count prev next))))))))
+
+(defun dump-unk-vocabulary (vocab out-path)
+  (let ((vocab-size (hachee.language-model.vocabulary::vocabulary-size
+                     vocab))
+        (to-int-map (hachee.language-model.vocabulary::vocabulary-to-int-map
+                     vocab))
+        (to-str-map (make-hash-table :test #'equal)))
+    (maphash (lambda (str int)
+               (assert (null (gethash int to-str-map)))
+               (setf (gethash int to-str-map) str))
+             to-int-map)
+    (with-open-file (out out-path :direction :output)
+      (dotimes (tok vocab-size)
+        (let ((str (hachee.kkc.impl.lm.unit:unit-form
+                    (gethash tok to-str-map))))
+          (format out "~A~A~A~%" tok #\tab str))))))
+
+(defun dump-unk-model (model vocab unk-ngram-counts-path)
+  (let ((freq (hachee.language-model.n-gram::model-freq model))
+        (vocab-size (hachee.language-model.vocabulary::vocabulary-size
+                     vocab)))
+    (with-open-file (out unk-ngram-counts-path :direction :output)
+      ;; 0-gram
+      (let ((count (hachee.language-model.freq:get-count freq nil)))
+        (format out "~A~%" count))
+      ;; 1-gram
+      (dotimes (token vocab-size)
+        (let ((count (hachee.language-model.freq:get-count
+                      freq (list token))))
+          (assert count)
+          (format out "~A,~A~%" count token)))
+      ;; 2-gram
+      (dotimes (prev vocab-size)
+        (dotimes (next vocab-size)
+          (let ((count (hachee.language-model.freq:get-count
+                        freq (list prev next))))
+            (when count
+              (format out "~A,~A,~A~%" count prev next))))))))
+
+(defun execute (kkc
+                class-vocab-path
+                class-weights-path
+                class-ngram-counts-path
+                class-word-counts-path
+                unk-vocab-path
+                unk-ngram-counts-path
+                char-set-size-path)
+  (dump-class-vocabulary
+   (kkc-vocabulary kkc)
+   (hachee.language-model.n-gram::class-model-classifier
+    (kkc-class-model kkc))
+   class-vocab-path)
+  (dump-class-model (kkc-class-model kkc)
+                    class-weights-path
+                    class-ngram-counts-path
+                    class-word-counts-path)
+  (dump-unk-vocabulary (kkc-unk-vocabulary kkc)
+                       unk-vocab-path)
+  (dump-unk-model (kkc-unk-model kkc)
+                  (kkc-unk-vocabulary kkc)
+                  unk-ngram-counts-path)
+  (alexandria:write-string-into-file "6878" char-set-size-path)
+  (values))
