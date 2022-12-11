f83717d782ec2764c41e2e2c29f7571fa3d4aa6a
mhkoji <mhkoji2+git@gmail.com>
2022-12-12 08:08:38 +0900

    fcitx: Add senn-bin-fcitx-server-stdio (#147)

---
+++ senn/src/bin/fcitx-server-stdio.lisp
@@ -0,0 +1,21 @@
+(defpackage :senn.bin.fcitx-server
+  (:use :cl)
+  (:export :main
+           :run))
+(in-package :senn.bin.fcitx-server)
+
+(defun start-server (ime)
+  (senn-ipc.server.stdio:start-server
+   (lambda (line)
+     (senn.fcitx.server:handle-request ime line))))
+
+(defun run (engine-path)
+  (let ((kkc (senn.im.kkc.engine:make-kkc-and-run
+              (senn.im.kkc.engine:make-engine-runner
+               :program engine-path))))
+    (unwind-protect
+         (start-server (senn.fcitx.stateful-ime:make-ime :kkc kkc))
+      (senn.im.kkc.engine:close-kkc kkc))))
+
+(defun main ()
+  (run (second sb-ext:*posix-argv*)))
