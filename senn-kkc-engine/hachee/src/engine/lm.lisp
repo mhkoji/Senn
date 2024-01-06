7ed59aec756e85dbb4d5b535cbf5a4394c8165bc
mhkoji <mhkoji2+git@gmail.com>
2024-01-06 14:05:16 +0900

    senn-kkc-engine: Call set-kkc when building an executable engine of hachee-lm

--- senn-kkc-engine/hachee/src/engine/lm.lisp
+++ senn-kkc-engine/hachee/src/engine/lm.lisp
@@ -1,6 +1,7 @@
 (defpackage :senn-kkc-engine.hachee.engine.lm
   (:use :cl)
   (:export :set-kkc
+           :build-kkc-using-hachee-corpus
            :main))
 (in-package :senn-kkc-engine.hachee.engine.lm)
 
@@ -8,6 +9,12 @@
 
 (defun set-kkc (kkc)
   (setq *kkc* kkc))
+
+(defun build-kkc-using-hachee-corpus ()
+  (let ((corpus-pathnames
+         (hachee.data.corpus:word-pron-utf8-pathnames)))
+    (log:debug "Loading: ~A" corpus-pathnames)
+    (hachee.kkc.impl.lm:build-kkc-simple corpus-pathnames)))
 
 (defun main ()
   (senn-ipc.server.stdio:start-server
