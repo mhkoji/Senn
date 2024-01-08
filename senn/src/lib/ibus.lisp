7b4c2e7a822a124433bf3c61198b837cd6b67344
mhkoji <mhkoji2+git@gmail.com>
2024-01-08 19:42:18 +0900

    senn-ibus: Resolve character encoding bug between ecl and c++

--- senn/src/lib/ibus.lisp
+++ senn/src/lib/ibus.lisp
@@ -1,7 +1,11 @@
 (defpackage :senn.lib.ibus
   (:use :cl)
-  (:import-from :senn.ibus.server
-                :handle-request)
   (:export :make-ime
            :close-ime
            :handle-request))
+(in-package :senn.lib.ibus)
+
+(defun handle-request (ime octets)
+  (let ((line (babel:octets-to-string octets :encoding :utf-8)))
+    (let ((output (senn.ibus.server:handle-request ime line)))
+      (babel:string-to-octets output :encoding :utf-8))))
