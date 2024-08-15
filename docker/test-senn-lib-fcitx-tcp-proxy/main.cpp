63640b18a87405dc2d5b44cba30624a772ae402f
mhkoji <mhkoji2+git@gmail.com>
2024-08-15 20:14:57 +0900

    senn: Refactor tcp proxy test

--- docker/test-senn-lib-fcitx-tcp-proxy/main.cpp
+++ docker/test-senn-lib-fcitx-tcp-proxy/main.cpp
@@ -1,16 +1,5 @@
 #include "fcitx/im/stateful_ime_ecl.h"
-#include <iostream>
-
-void PrintConverting(const senn::fcitx::im::views::Converting *view) {
-  std::cout << "Converting:";
-  for (size_t i = 0; i < view->forms.size(); i++) {
-    std::cout << " " << view->forms[i];
-  }
-}
-
-void PrintEditing(const senn::fcitx::im::views::Editing *view) {
-  std::cout << "Editing: " << view->input;
-}
+#include "fcitx/im/test.h"
 
 int main(int argc, char *argv[]) {
   if (argc < 2) {
@@ -23,17 +12,7 @@ int main(int argc, char *argv[]) {
   senn::fcitx::im::StatefulIME *ime =
       senn::fcitx::im::StatefulIMEEcl::Create(std::string(argv[1]));
 
-  std::vector<uint32_t> syms = {116, 111, 117, 107, 121, 111, 117,
-                                110, 105, 105, 107, 105, 109, 97,
-                                115, 105, 116, 97,  32};
-
-  std::cout << std::endl;
-  for (size_t i = 0; i < syms.size(); i++) {
-    uint32_t sym = syms[i];
-    std::cout << "Sym: " << sym << " -> ";
-    ime->ProcessInput(sym, 0, 0, PrintConverting, PrintEditing);
-    std::cout << std::endl;
-  }
+  senn::fcitx::im::Test(std::cin, ime);
 
   delete ime;
 
