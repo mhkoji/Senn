97aeab4b27ee2c62c6e85f6ce60b991e01f561d8
mhkoji <mhkoji2+git@gmail.com>
2024-08-15 20:55:34 +0900

    senn: Refactor test script

--- docker/test-senn-lib-fcitx/test/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -eux
-
-CURR_DIR=$(cd $(dirname $0) && pwd)
-for f in `ls "$CURR_DIR/testcases"`; do
-    echo "Testing ... $f"
-    cat "$CURR_DIR/testcases/$f" | "$CURR_DIR/main" "$CURR_DIR/kkc-engine-goto-tokyo.py"
-done
