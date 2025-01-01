51db9a67cb694f52316fc53be30c21467e58fb96
mhkoji <mhkoji2+git@gmail.com>
2025-01-01 23:30:18 +0900

    hachee: Refactor viterbi-2nd

--- hachee/src/kkc/convert/convert.lisp
+++ hachee/src/kkc/convert/convert.lisp
@@ -8,7 +8,10 @@
            :convert-end-entry
            :convert-score-fn
            :convert-list-entries-fn
-           :execute))
+           :execute
+
+           :convert-viterbi-2nd-score-fn
+           :viterbi-2nd))
 (in-package :hachee.kkc.convert)
 
 (defgeneric entry-form (e))
@@ -20,10 +23,20 @@
 (defgeneric convert-score-fn (mixin))
 (defgeneric convert-list-entries-fn (mixin))
 
+(defgeneric convert-viterbi-2nd-score-fn (mixin))
+
 (defun execute (convert pronunciation &key 1st-boundary-index)
   (hachee.kkc.convert.viterbi:execute pronunciation
    :begin-entry (convert-begin-entry convert)
    :end-entry (convert-end-entry convert)
    :score-fn (convert-score-fn convert)
+   :list-entries-fn (convert-list-entries-fn convert)
+   :1st-boundary-index 1st-boundary-index))
+
+(defun viterbi-2nd (convert pronunciation &key 1st-boundary-index)
+  (hachee.kkc.convert.viterbi-2nd:execute pronunciation
+   :begin-entry (convert-begin-entry convert)
+   :end-entry (convert-end-entry convert)
+   :score-fn (convert-viterbi-2nd-score-fn convert)
    :list-entries-fn (convert-list-entries-fn convert)
    :1st-boundary-index 1st-boundary-index))
